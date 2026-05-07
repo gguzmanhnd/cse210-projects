1718574269aa1fb71ef9c7b2e23176914bb8ff71
gguzmanhnd <gegzocp@gmail.com>
2026-05-07 14:49:16 -0600

    tarea

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -7,23 +7,23 @@ class Program
     {
         List<int> numbers = new List<int>();
         
-        // Please note we could use a do-while loop here instead
-        int userNumber = -1;
-        while (userNumber != 0)
+        
+        int input = -1;
+        while (input != 0)
         {
             Console.Write("Enter a number (0 to quit): ");
             
-            string userResponse = Console.ReadLine();
-            userNumber = int.Parse(userResponse);
+            string valueUser = Console.ReadLine();
+            input = int.Parse(valueUser);
             
-            // Only add the number to the list if it is not 0
-            if (userNumber != 0)
+          
+            if (input != 0)
             {
-                numbers.Add(userNumber);
+                numbers.Add(input);
             }
         }
 
-        // Part 1: Compute the sum
+       
         int sum = 0;
         foreach (int number in numbers)
         {
@@ -32,18 +32,10 @@ class Program
 
         Console.WriteLine($"The sum is: {sum}");
 
-        // Part 2: Compute the average
-        // Notice that we first cast the sum variable to be a float. Otherwise, because
-        // both the sum and the count are integers, the computer will do integer division
-        // and I will not get a decimal value (even though it puts the result into a float variable).
-
-        // By making one of the variables a float first, the computer knows that it has to
-        // do the floating point division, and we get the decimal value that we expect.
+        
         float average = ((float)sum) / numbers.Count;
         Console.WriteLine($"The average is: {average}");
 
-        // Part 3: Find the max
-        // There are several ways to do this, such as sorting the list
         
         int max = numbers[0];
 
@@ -51,7 +43,7 @@ class Program
         {
             if (number > max)
             {
-                // if this number is greater than the max, we have found the new max!
+                
                 max = number;
             }
         }
