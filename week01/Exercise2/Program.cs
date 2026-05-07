6d049a71fb9c4199459b684917a341b8a955c08c
gguzmanhnd <gegzocp@gmail.com>
2026-05-07 13:59:22 -0600

    list

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,37 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Write a program that determines the letter grade for a course.");
+        Console.WriteLine("");
+        Console.Write("What is your grade percentage?");
+        string userinput = Console.ReadLine(); 
+        int grade = int.Parse(userinput);
+        string letter = "";
+        // Console.WriteLine($"Your grade is: {grade}."); I use this line for testing :)!
+        if (grade >= 97)
+       // {Console.WriteLine("Your score is an: A"); I use this line for testing :)!
+            {
+            letter = "A";
+                }
+        else if (grade >= 80 && grade <97 )
+        //{Console.WriteLine("Your score is an: B"); I use this line for testing :)!
+        { letter = "B";
+        }     
+        else if (grade >= 70 && grade < 80)
+        //{Console.WriteLine("Your score is an C"); I use this line for testing :)!
+        { letter = "C";
+                }        
+        else if (grade >= 60 && grade < 70)
+        //{Console.WriteLine("Your score is an D"); I use this line for testing :)!
+        {   letter = "D";
+                }        
+        else if (grade < 60)
+        //{Console.WriteLine("Your score is an F"); I use this line for testing :)!
+        {   letter = "F";
+                }   
+        Console.WriteLine($"Your letter grade is: {letter}");                     
+        if (grade <70)
+        {Console.WriteLine("You must have at least a 70 to pass the class, so this time you did not make, but we encourage you to come back");}
+        else  {Console.WriteLine("Congratulations you have pass the class");}
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicnumber = randomGenerator.Next(1, 11);
+            
+        int guess = -1;
+
+        while (guess != magicnumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+
+            if (magicnumber == guess)
+            {
+                Console.Write("You guessed it!");
+            }
+            else if (guess < magicnumber)
+            {
+                Console.Write("Higher ");
+            }
+            else {Console.Write("Lower ");
+            
+            }
+        }
     }
 }
